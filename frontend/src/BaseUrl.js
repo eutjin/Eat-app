6ccfd0266dd0857508fe02d0d86f3615466e8d7b
eutjin <91615591+eutjin@users.noreply.github.com>
2022-12-31 22:50:14 +0900

    fix build

--- frontend/src/BaseUrl.js
+++ frontend/src/BaseUrl.js
@@ -1,5 +1,5 @@
-const baseUrl = process.env.REACT_APP_NODE_ENV === "production"
-? "https://dolphin-app-4xfzu.ondigitalocean.app/"
+const baseUrl = process.env.NODE_ENV === "production"
+? "https://dolphin-app-4xfzu.ondigitalocean.app"
 : "http://localhost:5000"
-console.log("ENV", process.env.REACT_APP_NODE_ENV)
+console.log("ENV", process.env.NODE_ENV)
 export default baseUrl;
--- frontend/src/BaseUrlFrontend.js
+++ frontend/src/BaseUrlFrontend.js
@@ -1,5 +1,5 @@
-const baseUrlFrontend = process.env.REACT_APP_NODE_ENV === "production"
+const baseUrlFrontend = process.env.NODE_ENV === "production"
 ? "https://dolphin-app-4xfzu.ondigitalocean.app"
 : "http://localhost:3000"
-console.log("ENV", process.env.REACT_APP_NODE_ENV)
+console.log("ENV", process.env.NODE_ENV)
 export default baseUrlFrontend;
